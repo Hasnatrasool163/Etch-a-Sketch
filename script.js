455b4f914558e6ba245818c79cc13a6590910161
YamanTariq <103046562+YamanTariq@users.noreply.github.com>
2025-01-25 16:27:40 +0500

    COMPLETE partial shade mode

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const eraserMdBtn = document.querySelector("#eraser-btn");
 const span = document.querySelector("#cur-size-span");
 
 let currentColor = "black"
-let bgColor = "white";
+let bgColor = "black";
 let option = 0 // 0: color mode, 1: rainbow, 2: shade, 3: eraser
 
 
@@ -62,15 +62,19 @@ grid.addEventListener("mouseover", (event)=>{
     if(event.target && event.target.classList.contains("cell") && mouseDown == true) {
         if(option === 0){
             event.target.style.backgroundColor = currentColor;
+            event.target.style.opacity = 1.0;
         } else if(option === 1){
             event.target.style.backgroundColor = getRainbowColor();
         } else if(option === 2){
-            const curOpacity = parseFloat(window.getComputedStyle(event.target).opacity) || 0;
-
+            var curOpacity = window.getComputedStyle(event.target).opacity;
+            curOpacity -= 0.1;
+            curOpacity += 0.1;
+            console.log(curOpacity);
             if(curOpacity < 1.0){
-                var result = curOpacity + 0.1;
-                event.target.style.opacity = result;
+                event.target.style.opacity = curOpacity + 0.1;
+
             }
+
         } else {
             event.target.style.backgroundColor = bgColor;           
         }
@@ -86,6 +90,7 @@ function createGrid(x){
         for(let j = 0; j < x; j++){
             const cell = document.createElement("div");
             cell.className = "cell";
+            cell.classList.add("opaque");
             cell.style.backgroundColor = bgColor;
             column.appendChild(cell);
         }
