4d7e318e802b2a034ed17671964affd520700db6
YamanTariq <103046562+YamanTariq@users.noreply.github.com>
2025-01-25 17:32:48 +0500

    UPDATE button structure
    updated sturcture to handle multiple options at a time

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
+
 const grid = document.querySelector(".grid");
 const slider = document.querySelector(".slider");
 const penColor = document.querySelector(".pen-color");
-const bgColorBtn = document.querySelector(".background-color");
 const clrBtn = document.querySelector("#clear");
-const shadeMdBtn = document.querySelector("#shade-btn");
-const colorMdBtn = document.querySelector("#pen-btn");
-const rainbowMdBtn = document.querySelector("#rainbow-btn");
-const eraserMdBtn = document.querySelector("#eraser-btn");
+const shade = document.querySelector("#shade-btn");
+const rainbow = document.querySelector("#rainbow-btn");
+const eraser = document.querySelector("#eraser-btn");
 const span = document.querySelector("#cur-size-span");
 
 let currentColor = "black"
-let bgColor = "black";
-let option = 0 // 0: color mode, 1: rainbow, 2: shade, 3: eraser
-
+let bgColor = "white";
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+let shadeMdBtn = false;
+let rainbowMdBtn = false; 
+let eraserMdBtn= false; 
 
 createGrid(slider.value);
-
-/* BUTTONS */
-colorMdBtn.addEventListener("click", () => {
-    option = 0; 
+//Buttons
+shade.addEventListener("click", () => {
+    shadeMdBtn = !shadeMdBtn;
+    shade.classList.toggle("selected");
 });
 
-rainbowMdBtn.addEventListener("click", () => {
-    option = 1;
+eraser.addEventListener("click", () => {
+    eraserMdBtn = !eraserMdBtn;
+    eraser.classList.toggle("selected");
 });
 
-shadeMdBtn.addEventListener("click", () => {
-    option = 2; 
-
-});
-
-eraserMdBtn.addEventListener("click", () => {
-    option = 3;
+rainbow.addEventListener("click", () => {
+    rainbowMdBtn= !rainbowMdBtn;
+    rainbow.classList.toggle("selected");
 });
 
 clrBtn.addEventListener("click", () => {
@@ -44,49 +41,47 @@ clrBtn.addEventListener("click", () => {
     createGrid(slider.value);
 });
 
-
 penColor.addEventListener("input", () =>{
    currentColor = penColor.value; 
 });
-
 
 slider.addEventListener("input", () =>{
     grid.innerHTML = "";
     createGrid(slider.value);
 });
 
-
 grid.addEventListener("mouseover", (event)=>{
-
-    
     if(event.target && event.target.classList.contains("cell") && mouseDown == true) {
-        if(option === 0){
+        if(eraserMdBtn){
+            event.target.style.backgroundColor = bgColor;           
+            event.target.style.opacity = 0;
+       } else if(rainbowMdBtn || shadeMdBtn){
+            if(rainbowMdBtn){
+                event.target.style.backgroundColor = getRainbowColor();
+            } else {
+                event.target.style.backgroundColor = currentColor;
+            }
+            if(shadeMdBtn){
+                var curOpacity = window.getComputedStyle(event.target).opacity;
+                curOpacity -= 0.1;
+                curOpacity += 0.1;
+                if(curOpacity < 1.0){
+                    event.target.style.opacity = curOpacity + 0.1;
+                }
+            } else {
+                event.target.style.opacity = 1.0;
+            }
+        } else {
             event.target.style.backgroundColor = currentColor;
             event.target.style.opacity = 1.0;
-        } else if(option === 1){
-            event.target.style.backgroundColor = getRainbowColor();
-        } else if(option === 2){
-            var curOpacity = window.getComputedStyle(event.target).opacity;
-            curOpacity -= 0.1;
-            curOpacity += 0.1;
-            console.log(curOpacity);
-            if(curOpacity < 1.0){
-                event.target.style.opacity = curOpacity + 0.1;
-
-            }
-
-        } else {
-            event.target.style.backgroundColor = bgColor;           
-        }
+        } 
     }
 });
-
 
 function createGrid(x){
     for(let i = 0; i < x; i++){
         const column = document.createElement("div");
         column.className = "column";
-
         for(let j = 0; j < x; j++){
             const cell = document.createElement("div");
             cell.className = "cell";
